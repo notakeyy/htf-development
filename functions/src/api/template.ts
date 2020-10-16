422872ca37a01449b972e27ff993964f0dc93402
Harsha Srikara <harshasrikara@gmail.com>
2020-10-16 17:24:26 -0500

    finish it up

--- functions/src/api/template.ts
+++ functions/src/api/template.ts
@@ -2,11 +2,13 @@ import { Request, Response } from "express";
 import * as functions from 'firebase-functions';
 import * as axios from "axios";
 
-const baseurl = functions.config().baseurl.firebase;
-
 export const first_last = async (request: Request, response: Response): Promise<void> => {
-    const email = "harshasrikara@gmail.com"
-    console.log(baseurl + email);
-    const result = await axios.default.get(baseurl + email);
-    response.json(result);
+    const baseurl = functions.config().baseurl.firebase;
+    const data = {
+        email: "harshasrikara@gmail.com"
+    }
+    const result = await (await axios.default.post(baseurl, data)).data;
+    response.json({
+        result: result
+    });
 }
